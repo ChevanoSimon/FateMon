e6a76b2acef783f12ba763aad98f8d7b3f0770ea
Chevano Simon <65667654+ChevanoSimon@users.noreply.github.com>
2020-08-12 21:20:15 +0200

    Add files via upload

---
+++ Scrips/Gameplay/MapArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapArea : MonoBehaviour
+{
+    [SerializeField] List<Servant> wildServants = default;
+
+    public Servant GetRandomWildServant()
+    {
+        var wildServant = wildServants[Random.Range(0, wildServants.Count)];
+        wildServant.Init();
+        return wildServant;
+    }
+
+}
