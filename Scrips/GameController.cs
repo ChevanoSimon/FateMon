9c00556600479bf45b8a62f2fc32f7f2ccb3b23c
Chevano Simon <65667654+ChevanoSimon@users.noreply.github.com>
2020-08-12 21:16:24 +0200

    Add files via upload

---
+++ Scrips/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { FreeRoam, Battle }
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField] PlayerController playerController = default;
+    [SerializeField] BattleSystem battleSystem = default;
+    [SerializeField] Camera worldCamera = default;
+
+    GameState state;
+
+    private void Start()
+    {
+        playerController.onEncounter += StartBattle;
+        battleSystem.onBattleOver += EndBattle;
+    }
+
+    void StartBattle()
+    {
+        state = GameState.Battle;
+        battleSystem.gameObject.SetActive(true);
+        worldCamera.gameObject.SetActive(false);
+
+        var servantParty = playerController.GetComponent<ServantParty>();
+        var wildServant = FindObjectOfType<MapArea>().GetComponent<MapArea>().GetRandomWildServant();
+
+        battleSystem.StartBattle(servantParty, wildServant);
+    }
+
+    void EndBattle(bool won)
+    {
+        state = GameState.FreeRoam;
+        battleSystem.gameObject.SetActive(false);
+        worldCamera.gameObject.SetActive(true);
+    }
+
+
+    public void Update()
+    {
+        if (state == GameState.FreeRoam)
+        {
+            playerController.HandleUpdate();
+        }
+
+        else if (state == GameState.Battle)
+        {
+            battleSystem.HandleUpdate();
+        }
+
+
+    }
+
+
+}
